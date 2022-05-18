9fae36caefabeef4e4f677d7b074c0526a2d1dd6
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-05-18 11:39:47 +0300

    sketching out chain and block

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Learning about blockchains...
 
 ## References
 * [Bitcoin: A Peer-to-Peer Electronic Cash System](https://bitcoin.org/bitcoin.pdf)
+* [Bitcoin scalability problem](https://en.wikipedia.org/wiki/Bitcoin_scalability_problem)
 * [Bitcoin Wiki](https://en.bitcoin.it/wiki/Main_Page)
+* [Blockchain](https://en.wikipedia.org/wiki/Blockchain)
 * [Hashcash - A Denial of Service Counter-Measure](http://www.hashcash.org/hashcash.pdf)
+* [HMAC: Keyed-Hashing for Message Authentication](https://datatracker.ietf.org/doc/html/rfc2104)
 * [Ledger](https://en.wikipedia.org/wiki/Ledger)
+* [Length extension attack](https://en.wikipedia.org/wiki/Length_extension_attack)
--- main.go
+++ main.go
@@ -1,19 +1,103 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+	"time"
 )
 
-func DoubleSha256(b []byte) []byte {
+type Block struct {
+	Index     uint64    `json:"index"`
+	Timestamp time.Time `json:"time"`
+	Data      int       `json:"data"`
+	PrevHash  []byte    `json:"prevHash"`
+	Hash      []byte    `json:"hash"`
+}
+
+type Blockchain struct {
+	Chain []*Block `json:"chain"`
+}
+
+func New() *Blockchain {
+	bc := new(Blockchain)
+	bc.Chain = make([]*Block, 0)
+	bc.addBlock(Sha256([]byte("genesis")), 0)
+	return bc
+}
+
+func (bc *Blockchain) hash(b *Block) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	var err error
+	encode := func(data any) {
+		if err == nil {
+			err = enc.Encode(data)
+		}
+	}
+
+	encode(b.Index)
+	encode(b.Timestamp)
+	encode(b.Data)
+	encode(b.PrevHash)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return Sha256(buf.Bytes()), nil
+}
+
+func (bc *Blockchain) getLastBlock() *Block {
+	return bc.Chain[len(bc.Chain)-1]
+}
+
+func (bc *Blockchain) addBlock(prevHash []byte, data int) error {
+	b := new(Block)
+	b.Index = uint64(len(bc.Chain))
+	b.Timestamp = time.Now().UTC()
+	b.Data = data
+	b.PrevHash = prevHash
+
+	// @TODO: Hashing should not return an error and
+	// should always succeed.
+	hash, err := bc.hash(b)
+	if err != nil {
+		return err
+	}
+
+	b.Hash = hash
+	bc.Chain = append(bc.Chain, b)
+
+	return nil
+}
+
+func (bc *Blockchain) isValidBlock(prev, block *Block) bool {
+	if !bytes.Equal(prev.Hash, block.PrevHash) || prev.Index+1 != block.Index {
+		return false
+	}
+
+	hash, _ := bc.hash(block)
+	return bytes.Equal(hash, block.Hash)
+}
+
+func Sha256(b []byte) []byte {
 	sha := sha256.New()
 	sha.Write(b)
-	intermediate := sha.Sum(nil)
-
-	sha.Reset()
-	sha.Write(intermediate)
 	return sha.Sum(nil)
 }
 
 func main() {
+	bc := New()
+	bc.addBlock(bc.getLastBlock().Hash, 1)
+	bc.addBlock(bc.getLastBlock().Hash, 2)
+	bc.addBlock(bc.getLastBlock().Hash, 3)
+	bc.addBlock(bc.getLastBlock().Hash, 4)
+	bc.addBlock(bc.getLastBlock().Hash, 5)
 
+	b, _ := json.MarshalIndent(bc, "", "  ")
+	fmt.Println(string(b))
 }
